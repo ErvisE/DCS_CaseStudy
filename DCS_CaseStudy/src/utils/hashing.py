405f56d2d3c9b526777cc6b476a6deeb2e16d5ae
ErvisE <ervis.elezi@ottogroup.com>
2025-11-23 15:46:16 +0100

    Delete DCS_CaseStudy directory

--- DCS_CaseStudy/src/utils/hashing.py
+++
@@ -1,8 +0,0 @@
-"""
-Utility functions for hashing and anonymisation.
-"""
-import hashlib
-
-def hash_user_id(uid: str) -> str:
-    """Return a SHA-256 hash of the user ID as a hex string."""
-    return hashlib.sha256(uid.encode("utf-8")).hexdigest()
